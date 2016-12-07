d9ea732ceb9ca3b1992fff2e9667ca1885f3389b
Piper Thunstrom <pathunstrom@gmail.com>
2016-12-07 00:30:10 -0500

    Adds setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name='ppb-vector',
+    version='0.1',
+    packages=['ppb_vector'],
+    url='http://github.com/pathunstrom/ppb-vector',
+    license='',
+    author='Piper Thunstrom',
+    author_email='pathunstrom@gmail.com',
+    description='A basic game development Vector2 class.',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5'
+    ]
+)
