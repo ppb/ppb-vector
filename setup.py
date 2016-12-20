4042898521568b075276648257ab81b0f67396a4
Piper Thunstrom <pathunstrom@gmail.com>
2016-12-20 04:45:16 -0500

    Updates version number for package.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='ppb-vector',
-    version='0.1',
+    version='0.2',
     packages=['ppb_vector'],
     url='http://github.com/pathunstrom/ppb-vector',
     license='',
