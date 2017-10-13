ae675473460cfef4fa55a2cf1339c9fcd033b6bd
Jeronimo F. Martinez Sanchez <jms@rz0r.net>
2017-10-12 20:08:33 -0600

    update fixture name

--- tests/test_vector2_member_access.py
+++ tests/test_vector2_member_access.py
@@ -3,20 +3,20 @@ from ppb_vector import Vector2
 
 
 @pytest.fixture()
-def test_vector():
+def vector():
     return Vector2(10, 20)
 
 
-def test_vector_2_class_member_access(test_vector):
-    assert test_vector.x == 10
-    assert test_vector.y == 20
+def vector_2_class_member_access(vector):
+    assert vector.x == 10
+    assert vector.y == 20
 
 
-def test_vector2_index_access(test_vector):
-    assert test_vector[0] == 10
-    assert test_vector[1] == 20
+def vector2_index_access(vector):
+    assert vector[0] == 10
+    assert vector[1] == 20
 
 
-def test_vector2_key_access(test_vector):
-    assert test_vector['x'] == 10
-    assert test_vector['y'] == 20
+def vector2_key_access(vector):
+    assert vector['x'] == 10
+    assert vector['y'] == 20
