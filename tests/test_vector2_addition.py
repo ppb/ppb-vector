539b1be094a1ec9937c6da340ae631e80d70a6c9
Jeronimo F. Martinez Sanchez <jms@rz0r.net>
2017-10-12 18:30:18 -0600

    added unit test for vector2 addition

---
+++ tests/test_vector2_addition.py
@@ -0,0 +1,31 @@
+import unittest
+import ppb_vector
+
+
+class TestVector2Addition(unittest.TestCase):
+    def test_addition_vectors(self):
+        test_vector1 = ppb_vector.Vector2(1, 0)
+        test_vector2 = ppb_vector.Vector2(0, 1)
+
+        result = test_vector1 + test_vector2
+        self.assertEqual(result, ppb_vector.Vector2(1, 1))
+
+    def test_addition_vector_tuple(self):
+        test_vector = ppb_vector.Vector2(1, 1)
+        test_tuple = (2, 4)
+        result = test_vector + test_tuple
+        self.assertEqual(result, ppb_vector.Vector2(3, 5))
+
+    def test_addition_vector_list(self):
+        test_vector = ppb_vector.Vector2(1, 1)
+        test_list = [1, 3]
+        result = test_vector + test_list
+        self.assertEqual(result, ppb_vector.Vector2(2, 4))
+
+    def test_addition_vector_dict(self):
+        test_vector = ppb_vector.Vector2(1, 1)
+        test_dict = {'x': 3, 'y': 5}
+        result = test_vector + test_dict
+        self.assertEqual(result, ppb_vector.Vector2(4, 6))
+
+
