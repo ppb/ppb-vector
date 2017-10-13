b8cb2da34aa7ce9f18ffeb40aed58141354307c5
Rodrigo Prado <royopa@gmail.com>
2017-10-12 21:53:42 -0300

    Updated test using pytest

--- tests/test_vector2_truncate.py
+++ tests/test_vector2_truncate.py
@@ -1,15 +1,8 @@
-import unittest
-
 import ppb_vector
 
 
-class TestTruncate(unittest.TestCase):
-
-    def test_truncate(self):
-        test_vector = ppb_vector.Vector2(700, 500)
-        test_vector_truncated = test_vector.truncate(5)
-        print(test_vector_truncated)
-        self.assertEqual(
-            test_vector_truncated,
-            ppb_vector.Vector2(4.068667356033675, 2.906190968595482)
-        )
+def test_truncate():
+    test_vector = ppb_vector.Vector2(700, 500)
+    test_vector_truncated = test_vector.truncate(5)
+    print(test_vector_truncated)
+    assert test_vector_truncated == ppb_vector.Vector2(4.068667356033675, 2.906190968595482)
