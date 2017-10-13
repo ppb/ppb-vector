1f0b0ffdb1b6884f68e4bc51d79dea28b81772b7
Rodrigo Prado <royopa@gmail.com>
2017-10-12 22:02:39 -0300

    Updated test using pytest style

--- tests/test_vector2.py
+++ tests/test_vector2.py
@@ -1,13 +1,9 @@
-import unittest
-
 import ppb_vector
 
 
-class TestIterable(unittest.TestCase):
-
-    def test_is_iterable(self):
-        test_vector = ppb_vector.Vector2(3, 4)
-        test_tuple = tuple(test_vector)
-        print(test_tuple)
-        self.assertEqual(test_tuple[0], 3)
-        self.assertEqual(test_tuple[1], 4)
+def test_is_iterable():
+    test_vector = ppb_vector.Vector2(3, 4)
+    test_tuple = tuple(test_vector)
+    print(test_tuple)
+    assert test_tuple[0] == 3
+    assert test_tuple[1] == 4
