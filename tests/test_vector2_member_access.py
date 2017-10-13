97de6daf434ab86c681e5da8554336b26e0a6f97
Jeronimo F. Martinez Sanchez <jms@rz0r.net>
2017-10-12 20:12:21 -0600

    fix test name

--- tests/test_vector2_member_access.py
+++ tests/test_vector2_member_access.py
@@ -7,16 +7,16 @@ def vector():
     return Vector2(10, 20)
 
 
-def vector_2_class_member_access(vector):
+def test_vector2_class_member_access(vector):
     assert vector.x == 10
     assert vector.y == 20
 
 
-def vector2_index_access(vector):
+def test_vector2_index_access(vector):
     assert vector[0] == 10
     assert vector[1] == 20
 
 
-def vector2_key_access(vector):
+def test_vector2_key_access(vector):
     assert vector['x'] == 10
     assert vector['y'] == 20
