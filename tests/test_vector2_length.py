af58c90dd2bd633b2e6c17b2b50cfb33593bd276
Amber Phair <amphair@outlook.com>
2017-10-13 11:13:07 -0400

    Correcting whitespace

--- tests/test_vector2_length.py
+++ tests/test_vector2_length.py
@@ -1,6 +1,7 @@
 import ppb_vector
 import pytest
 
+
 @pytest.mark.parametrize("x, y, expected", [
     (6, 8, 10),
     (8, 6, 10),
@@ -8,7 +9,6 @@ import pytest
     (-6, -8, 10),
     (1, 2, 2.23606797749979)
 ])
-
 def test_length(x, y, expected):
     vector = ppb_vector.Vector2(x, y)
     assert vector.length == expected
