ce11524b5df4441fe7278b19771ea85c8874b3ee
Nicolas Braud-Santoni <nicolas@braud-santoni.eu>
2018-10-12 01:30:19 -0400

    Add an Hypothesis strategy for generating unit (length-1) vectors

--- tests/test_vector2_reflect.py
+++ tests/test_vector2_reflect.py
@@ -25,19 +25,22 @@ stvector = lambda: st.builds(
     st.floats(allow_nan=False, allow_infinity=False)
 )
 
+@st.composite
+def stunit(draw, elements=st.floats(min_value=0, max_value=360)):
+    angle = draw(elements)
+    return Vector2(1, 0).rotate(angle)
+
 
 def isclose_vector(a, b, *, rel_tol=1e-06, abs_tol=1e-3):
     d = (a - b).length
     return d < rel_tol * max(a.length, b.length) or d < abs_tol
 
 
-@given(initial=stvector(), normal=stvector())
+@given(initial=stvector(), normal=stunit())
 def test_reflect_prop(initial: Vector2, normal: Vector2):
     assume(initial != Vector2(0, 0))
-    assume(normal != Vector2(0, 0))
     assume(initial ^ normal != 0)
     assume(initial.length < 1e300)
-    normal = normal.normalize()
     reflected = initial.reflect(normal)
     returned = reflected.reflect(normal)
     note(f"Reflected: {reflected}")
