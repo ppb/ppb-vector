14e3fe43f2687b5944576e60003bf538b4fc0bd8
Jamie Bliss <astronouth7303@gmail.com>
2018-10-12 23:26:03 -0400

    Add tests for angle

---
+++ tests/test_vector2_angle.py
@@ -0,0 +1,14 @@
+from ppb_vector import Vector2
+from math import isclose
+import pytest
+
+@pytest.mark.parametrize("left, right, expected", [
+    (Vector2(1, 1), Vector2(0, -1), 135),
+    (Vector2(1, 1), Vector2(-1, 0), 135),
+    (Vector2(0, 1), Vector2(0, -1), 180),
+    (Vector2(-1, -1), Vector2(1, 0), 135),
+    (Vector2(-1, -1), Vector2(-1, 0), 45)
+])
+def test_angle(left, right, expected):
+    assert isclose(left.angle(right), expected)
+    assert isclose(right.angle(left), expected)
