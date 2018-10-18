cd9a006b7d3773dc84e1436760f35c022efa2fd8
Nicolas Braud-Santoni <nicolas@braud-santoni.eu>
2018-10-17 23:47:06 -0400

    Provide annotations for the cross-product tests

--- tests/test_vector2_cross.py
+++ tests/test_vector2_cross.py
@@ -1,5 +1,5 @@
 from ppb_vector import Vector2
-from hypothesis import given, strategies as st
+from hypothesis import given, note, strategies as st
 from math import isclose
 from numbers import Real
 from utils import vectors
@@ -22,11 +22,19 @@ def test_cross(left, right, expected):
        l=st.floats(min_value=-1e75,max_value=1e75),
 )
 def test_cross_linearity(a: Vector2, b: Vector2, c: Vector2, l: Real):
-    assert isclose(
-        a ^ (l * b + c),
-        l * (a ^ b) + (a ^ c)
-    )
+    factored = a ^ (l * b + c)
+    distributed = l * (a^b) + (a^c)
+
+    note(f"a ^ (l * b + c): {factored}")
+    note(f"l * (a^b) + (a^c): {distributed}")
+    assert isclose(factored, distributed)
 
 @given(a=vectors(max_magnitude=1e150), b=vectors(max_magnitude=1e150))
 def test_cross_antisymetry(a: Vector2, b: Vector2):
-    assert isclose(a ^ b, - b ^ a)
+    prod1 = a ^ b
+    prod2 = b ^ a
+
+    note(f"a ^ b: {prod1}")
+    note(f"b ^ a: {prod2}")
+    note(f"δ: {prod1 + prod2}")
+    assert isclose(prod1, -prod2)
