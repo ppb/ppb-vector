0cee3a210e37808400941e09546dab31248bdca1
Jamie Bliss <astronouth7303@gmail.com>
2018-10-20 21:05:21 -0400

    test_typing: Use more explicit constant names

--- tests/test_typing.py
+++ tests/test_typing.py
@@ -3,14 +3,14 @@ import pytest
 from ppb_vector import Vector2
 
 # List of operations that (Vector2, Vector2) -> Vector2
-BINOPS = [
+BINARY_OPS = [
     Vector2.__add__,
     Vector2.__sub__,
     Vector2.reflect,
 ]
 
 # List of operations that (Vector2, Real) -> Vector2
-VNUMOPS = [
+VECTOR_NUMBER_OPS = [
     Vector2.__mul__,
     Vector2.__rmul__,
     Vector2.rotate,
@@ -19,13 +19,13 @@ VNUMOPS = [
 ]
 
 # List of operations that (Vector2) -> Vector2
-MONOPS = [
+UNARY_OPS = [
     Vector2.convert,
     Vector2.__neg__,
     Vector2.normalize,
 ]
 
-@pytest.mark.parametrize('op', BINOPS)
+@pytest.mark.parametrize('op', BINARY_OPS)
 def test_binop_same(op):
     class V(Vector2): pass
 
@@ -35,7 +35,7 @@ def test_binop_same(op):
     assert isinstance(a, V)
 
 
-@pytest.mark.parametrize('op', BINOPS)
+@pytest.mark.parametrize('op', BINARY_OPS)
 def test_binop_different(op):
     class V1(Vector2): pass
     class V2(Vector2): pass
@@ -47,7 +47,7 @@ def test_binop_different(op):
     assert isinstance(b, (V1, V2))
 
 
-@pytest.mark.parametrize('op', BINOPS)
+@pytest.mark.parametrize('op', BINARY_OPS)
 def test_binop_subclass(op):
     class V1(Vector2): pass
     class V2(V1): pass
@@ -59,7 +59,7 @@ def test_binop_subclass(op):
     assert isinstance(b, V2)
 
 
-@pytest.mark.parametrize('op', VNUMOPS)
+@pytest.mark.parametrize('op', VECTOR_NUMBER_OPS)
 def test_vnumop(op):
     class V(Vector2): pass
     
@@ -68,7 +68,7 @@ def test_vnumop(op):
     assert isinstance(a, V)
 
 
-@pytest.mark.parametrize('op', MONOPS)
+@pytest.mark.parametrize('op', UNARY_OPS)
 def test_monop(op):
     class V(Vector2): pass
     
