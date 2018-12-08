273a9e427b75a33e25a08a00cc4225177a249b40
Nicolas Braud-Santoni <nicolas@braud-santoni.eu>
2018-12-08 21:06:39 +0100

    Remove test_calculate_scale
    It was a fairly pointless test, as it used the same code
    ((scale / x.length) * x) as the actual implementation.

--- tests/test_vector2_scale.py
+++ tests/test_vector2_scale.py
@@ -1,23 +1,9 @@
 import pytest  # type: ignore
 from hypothesis import assume, given
 from hypothesis.strategies import floats
-from math import hypot
 from utils import vectors
 
 from ppb_vector import Vector2
-
-
-@pytest.fixture()
-def vector():
-    return Vector2(10, 20)
-
-
-def test_calculate_scale(vector):
-    x, y = 10, 20
-    length = hypot(x, y)
-    scale = 4
-    vector_scale_calculated = vector * (scale / length)
-    assert vector.scale(scale) == vector_scale_calculated
 
 
 @given(x=vectors(), l=floats(min_value=1e150, max_value=1e150))
