e131ef5b7ed7df341d23c1b295a9d6caef5b0fe6
Nicolas Braud-Santoni <nicolas@braud-santoni.eu>
2018-12-10 16:58:13 +0100

    Vector2.angle: Test on collinear vectors

--- tests/test_vector2_angle.py
+++ tests/test_vector2_angle.py
@@ -2,6 +2,7 @@ from ppb_vector import Vector2
 from math import isclose
 import pytest  # type: ignore
 from hypothesis import assume, given, note
+from hypothesis.strategies import floats
 from utils import angle_isclose, vectors
 
 
@@ -44,3 +45,12 @@ def test_angle_additive(left, middle, right):
     mr = middle.angle(right)
     lr = left.angle(right)
     assert angle_isclose(lm + mr, lr)
+
+@given(
+    x=vectors(max_magnitude=1e150),
+    l=floats(min_value=-1e150, max_value=1e150),
+)
+def test_angle_aligned(x: Vector2, l: float):
+    assume(l != 0)
+    y = l * x
+    assert angle_isclose(x.angle(y), 0 if l > 0 else 180)
