b115ef5b7f7e3d377a50339dfd2d237df707573c
Nicolas Braud-Santoni <nicolas@braud-santoni.eu>
2018-12-14 11:09:59 +0100

    Add: Add a perf-based benchmark

---
+++ tests/benchmark.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+import perf
+from ppb_vector import Vector2
+from utils import *
+
+r = perf.Runner()
+x = Vector2(1, 1)
+y = Vector2(0, 1)
+scalar = 123
+
+for f in BINARY_OPS + BINARY_SCALAR_OPS + BOOL_OPS:
+    r.bench_func(f.__name__, f, x, y)
+
+for f in UNARY_OPS + UNARY_SCALAR_OPS:
+    r.bench_func(f.__name__, f, x)
+
+for f in SCALAR_OPS:
+    r.bench_func(f.__name__, f, x, scalar)
