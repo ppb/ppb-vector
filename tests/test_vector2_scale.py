96b3911faadc22a07176c9338420ac8cd9fb06e5
Nicolas Braud-Santoni <nicolas@braud-santoni.eu>
2018-12-21 18:56:12 +0100

    Test that scaling doesn't rotate vectors

--- tests/test_vector2_scale.py
+++ tests/test_vector2_scale.py
@@ -2,7 +2,7 @@ import pytest  # type: ignore
 from hypothesis import assume, given
 from hypothesis.strategies import floats
 from math import isclose
-from utils import vectors
+from utils import angle_isclose, vectors
 
 from ppb_vector import Vector2
 
@@ -16,3 +16,13 @@ def test_scale_to_length(x: Vector2, l: float):
         assert x == (0, 0)
     except ValueError:
         assert l < 0
+
+
+@given(x=vectors(max_magnitude=1e75), length=floats(min_value=0, max_value=1e75))
+def test_scale_aligned(x: Vector2, length: float):
+    """Test that the length of x.scale_to(length) is length."""
+    assume(length > 0)
+    try:
+        assert angle_isclose(x.scale_to(length).angle(x), 0)
+    except ZeroDivisionError:
+        assert x == (0, 0)
