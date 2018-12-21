4bcca1d22c99d66755464bac3cb35da70ee8255b
Nicolas Braud-Santoni <nicolas@braud-santoni.eu>
2018-12-21 18:47:35 +0100

    Remove unit tests checking equivalence of truncate and scale
    The Hypothesis test is a more thorough way to do that anyhow.

--- tests/test_vector2_truncate.py
+++ tests/test_vector2_truncate.py
@@ -14,25 +14,6 @@ def test_truncate():
     assert test_vector_truncated == Vector2(4.068667356033675, 2.906190968595482)
 
 
-def test_truncate_lesser_max_length():
-    vector = Vector2(20, 30)
-    truncated = vector.truncate(10)
-    assert truncated == vector.scale(10)
-
-
-data = [
-    ([Vector2(5, 12), 6], Vector2(5, 12).scale(6)),
-    ([Vector2(92, 19), 61], Vector2(92, 19).scale(61)),
-    ([Vector2(2212481, 189898), 129039], Vector2(2212481, 189898).scale(129039)),
-    ([Vector2(155, 155), 155], Vector2(155, 155).scale(155))
-]
-
-
-@pytest.mark.parametrize('test_input, expected', data)
-def test_multiples_values(test_input, expected):
-    assert test_input[0].truncate(test_input[1]) == expected
-
-
 @given(x=vectors(max_magnitude=1e75), max_length=floats(min_value=0, max_value=1e75))
 def test_truncate_length(x: Vector2, max_length: float):
     assert x.truncate(max_length).length <= max_length
