2142db8de793382bdfc56de9133b320b5d6f2690
Nicolas Braud-Santoni <nicolas@braud-santoni.eu>
2018-12-29 15:24:14 +0100

    Test Vector2.isclose with invalid values

--- tests/test_vector2_isclose.py
+++ tests/test_vector2_isclose.py
@@ -1,4 +1,5 @@
 from ppb_vector import Vector2
+from pytest import raises # type: ignore
 from utils import vectors
 from hypothesis import assume, given, note, example
 from hypothesis.strategies import floats
@@ -7,3 +8,13 @@ from hypothesis.strategies import floats
 @given(x=vectors(), abs_tol=floats(min_value=0), rel_tol=floats(min_value=0))
 def test_isclose_to_self(x, abs_tol, rel_tol):
     assert x.isclose(x, abs_tol=abs_tol, rel_tol=rel_tol)
+
+
+def test_isclose_negative_tolerances():
+    zero = Vector2(0, 0)
+
+    with raises(ValueError):
+        zero.isclose(zero, abs_tol=-1)
+
+    with raises(ValueError):
+        zero.isclose(zero, rel_tol=-1)
