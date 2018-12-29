a4ced4b9dcaa87a811b8608940e34a1e16cad366
Nicolas Braud-Santoni <nicolas@braud-santoni.eu>
2018-12-29 15:25:47 +0100

    Vector2.isclose: Throw ValueError on invalid tolerances

--- ppb_vector/vector2.py
+++ ppb_vector/vector2.py
@@ -231,6 +231,9 @@ class Vector2:
         For the values to be considered close, the difference between them
         must be smaller than at least one of the tolerances.
         """
+        if abs_tol < 0 or rel_tol < 0:
+            raise ValueError("Vector2.isclose takes non-negative tolerances")
+
         other = Vector2.convert(other)
 
         rel_length = max(
