f66c13fa9bbf9a9e8dbfa082a0c15440f1eaf376
Nicolas Braud-Santoni <nicolas@braud-santoni.eu>
2019-01-02 16:12:45 +0100

    tests/typing: Extract subclass definitions to the file level

--- tests/test_typing.py
+++ tests/test_typing.py
@@ -3,21 +3,30 @@ import pytest  # type: ignore
 from ppb_vector import Vector2
 from utils import *
 
+
+class V1(Vector2):
+    """Arbitrary subclass of Vector2."""
+    pass
+
+class V11(V1):
+    """Subclass of V1."""
+    pass
+
+class V2(Vector2):
+    """Arbitrary subclass of Vector2, distinct from V1."""
+    pass
+
+
 @pytest.mark.parametrize('op', BINARY_OPS)
 def test_binop_same(op):
-    class V(Vector2): pass
-
     # Normalize for reflect
-    a = op(V(1, 2), V(3, 4).normalize())
+    a = op(V1(1, 2), V1(3, 4).normalize())
 
-    assert isinstance(a, V)
+    assert isinstance(a, V1)
 
 
 @pytest.mark.parametrize('op', BINARY_OPS)
 def test_binop_different(op):
-    class V1(Vector2): pass
-    class V2(Vector2): pass
-
     # Normalize for reflect
     a = op(V1(1, 2), V2(3, 4).normalize())
     b = op(V2(1, 2), V1(3, 4).normalize())
@@ -27,32 +36,23 @@ def test_binop_different(op):
 
 @pytest.mark.parametrize('op', BINARY_OPS)
 def test_binop_subclass(op):
-    class V1(Vector2): pass
-    class V2(V1): pass
-
     # Normalize for reflect
-    a = op(V1(1, 2), V2(3, 4).normalize())
-    b = op(V2(1, 2), V1(3, 4).normalize())
-    assert isinstance(a, V2)
-    assert isinstance(b, V2)
+    a = op(V1(1, 2), V11(3, 4).normalize())
+    b = op(V11(1, 2), V1(3, 4).normalize())
+    assert isinstance(a, V11)
+    assert isinstance(b, V11)
 
 
 @pytest.mark.parametrize('op', SCALAR_OPS)
 def test_vnumop(op):
-    class V(Vector2): pass
-
-    a = op(V(1, 2), 42)
-
-    assert isinstance(a, V)
+    a = op(V1(1, 2), 42)
+    assert isinstance(a, V1)
 
 
 @pytest.mark.parametrize('op', UNARY_OPS)
 def test_monop(op):
-    class V(Vector2): pass
-
-    a = op(V(1, 2))
-
-    assert isinstance(a, V)
+    a = op(V1(1, 2))
+    assert isinstance(a, V1)
 
 
 @pytest.mark.parametrize('op', BINARY_OPS + BINARY_SCALAR_OPS + BOOL_OPS) # type: ignore
