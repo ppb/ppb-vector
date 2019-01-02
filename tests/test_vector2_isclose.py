d5cb48571d801b219d1a469d26e21141fa276c26
Nicolas Braud-Santoni <nicolas@braud-santoni.eu>
2019-01-02 10:14:04 +0100

    tests/isclose: Use common strategies from the utils package

--- tests/test_vector2_isclose.py
+++ tests/test_vector2_isclose.py
@@ -1,6 +1,6 @@
 from ppb_vector import Vector2
 from pytest import raises # type: ignore
-from utils import units, vectors
+from utils import units, lengths, vectors
 from hypothesis import assume, given, note, example
 from hypothesis.strategies import floats
 
@@ -9,8 +9,8 @@ from hypothesis.strategies import floats
 def test_isclose_to_self(x, abs_tol, rel_tol):
     assert x.isclose(x, abs_tol=abs_tol, rel_tol=rel_tol)
 
-@given(x=vectors(max_magnitude=1e75), direction=units(),
-       abs_tol=floats(min_value=0, max_value=1e75))
+
+@given(x=vectors(), direction=units(), abs_tol=lengths())
 def test_isclose_abs_error(x, direction, abs_tol):
     assert x.isclose(x + (1 - 1e-12) * abs_tol * direction, abs_tol=abs_tol, rel_tol=0)
 
