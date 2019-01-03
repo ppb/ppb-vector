75da1d34aad60599d101b57577faee5436d86d9c
Nicolas Braud-Santoni <nicolas@braud-santoni.eu>
2019-01-03 18:24:20 +0100

    tests/convert: Parameterize by Vector2 (sub)class
    Also remove obsoleted tests from tests/test_vector2.py

--- tests/test_vector2.py
+++ tests/test_vector2.py
@@ -18,28 +18,3 @@ negation_data = (
 @pytest.mark.parametrize('test_vector, expected_result', negation_data)
 def test_negation(test_vector, expected_result):
     assert -test_vector == expected_result
-
-
-@pytest.mark.parametrize('value', [
-    Vector2(1, 2),
-    [3, 4],
-    (5, 6),
-    {'x': 7, 'y': 8},
-])
-def test_convert(value):
-    v = Vector2.convert(value)
-    assert isinstance(v, Vector2)
-    assert v == value
-
-
-@pytest.mark.parametrize('value', [
-    Vector2(1, 2),
-    [3, 4],
-    (5, 6),
-    {'x': 7, 'y': 8},
-])
-def test_convert_subclass(value):
-    class V(Vector2): pass
-    v = V.convert(value)
-    assert isinstance(v, V)
-    assert v == value
