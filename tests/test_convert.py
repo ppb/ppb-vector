3c520a3796db353f98932dcadcc107a419356367
Nicolas Braud-Santoni <nicolas@braud-santoni.eu>
2019-01-03 18:24:20 +0100

    tests/convert: Parameterize by Vector2 (sub)class
    Also remove obsoleted tests from tests/test_vector2.py

--- tests/test_convert.py
+++ tests/test_convert.py
@@ -3,10 +3,11 @@ import pytest  # type: ignore
 from ppb_vector import Vector2
 from utils import vector_likes
 
-@pytest.mark.parametrize('vector_like', vector_likes(), ids=lambda x: type(x).__name__) # type: ignore
-def test_convert_subclass(vector_like):
-    class V(Vector2): pass
+class V(Vector2): pass
 
-    vector = V.convert(vector_like)
-    assert isinstance(vector, V)
+@pytest.mark.parametrize('vector_like', vector_likes(), ids=lambda x: type(x).__name__) # type: ignore
+@pytest.mark.parametrize('cls', [Vector2, V]) # type: ignore
+def test_convert_class(cls, vector_like):
+    vector = cls.convert(vector_like)
+    assert isinstance(vector, cls)
     assert vector == vector_like
