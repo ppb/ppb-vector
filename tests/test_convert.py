c9be4b71cc211d15c2b5a5f495ccc60385aae9e9
Nicolas Braud-Santoni <nicolas@braud-santoni.eu>
2019-01-03 18:16:48 +0100

    types/convert: Provide useful ids for vector-likes in test name

--- tests/test_convert.py
+++ tests/test_convert.py
@@ -3,7 +3,7 @@ import pytest  # type: ignore
 from ppb_vector import Vector2
 from utils import vector_likes
 
-@pytest.mark.parametrize('vector_like', vector_likes()) # type: ignore
+@pytest.mark.parametrize('vector_like', vector_likes(), ids=lambda x: type(x).__name__) # type: ignore
 def test_convert_subclass(vector_like):
     class V(Vector2): pass
 
