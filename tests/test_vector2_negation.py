76bda324fcd617677a3f107e6b7c162a81e88db9
Nicolas Braud-Santoni <nicolas@braud-santoni.eu>
2019-01-06 00:02:39 +0100

    tests/negation: Replace with an Hypothesis test

--- tests/test_vector2_negation.py
+++ tests/test_vector2_negation.py
@@ -1,15 +1,9 @@
-import pytest
+from hypothesis import given
 
 from ppb_vector import Vector2
+from utils import vectors
 
-
-negation_data = (
-    (Vector2(1, 1), Vector2(-1, -1)),
-    (Vector2(2, -3), Vector2(-2, 3)),
-    (Vector2(-4, 18), Vector2(4, -18))
-)
-
-
-@pytest.mark.parametrize('test_vector, expected_result', negation_data)
-def test_negation(test_vector, expected_result):
-    assert -test_vector == expected_result
+@given(vector=vectors())
+def test_negation_coordinates(vector: Vector2):
+    assert - vector.x == (- vector).x
+    assert - vector.y == (- vector).y
