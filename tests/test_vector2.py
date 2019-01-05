ca0dec4321a61656c3972b79e7a074f83f285b1c
Nicolas Braud-Santoni <nicolas@braud-santoni.eu>
2019-01-05 23:42:13 +0100

    tests/convert: Move the tuple-conversion test & use Hypothesis

--- tests/test_vector2.py
+++
@@ -1,9 +0,0 @@
-import pytest  # type: ignore
-
-from ppb_vector import Vector2
-
-
-def test_is_iterable():
-    test_vector = Vector2(3, 4)
-    test_tuple = tuple(test_vector)
-    assert test_tuple == (3, 4)
