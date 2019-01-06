2a55886dc02a9b300cfa2d8d69ce49ac53abceee
Nicolas Braud-Santoni <nicolas@braud-santoni.eu>
2019-01-06 14:25:00 +0100

    Travis: Do the Python install in a separate script
    Also, remove the need for MINICONDA_OS in the Travis build matrix definition.

---
+++ .ci/install-python.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+set -euxo pipefail
+
+# Nothing to do if no specific Python version is requested
+[ -n "${PYTHON+x}" ] || return 0
+
+if [[ "$TRAVIS_OS_NAME" != linux ]]; then
+   echo "This script only supports linux, got \$TRAVIS_OS_NAME='${TRAVIS_OS_NAME}'" >&2
+   exit 1
+fi
+
+MINICONDA_OS=Linux
+URL="https://repo.continuum.io/miniconda/Miniconda3-latest-${MINICONDA_OS}-x86_64.sh"
+wget "${URL}" -O miniconda.sh
+bash miniconda.sh -b -p "$HOME/miniconda"
+export PATH="$HOME/miniconda/bin:$PATH"
+conda config --set always_yes yes --set changeps1 no
+conda update -q conda
+conda info -a
+conda create -q -n test-environment "python=$PYTHON"
+source activate test-environment
