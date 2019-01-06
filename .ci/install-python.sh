5894608cc6c639d632f9c605f61ad5236595f038
Nicolas Braud-Santoni <nicolas@braud-santoni.eu>
2019-01-06 14:46:47 +0100

    Travis: Add PyPy builds
    matrix.fast_finish ensures this won't slow down CI runs, as the CI will go
    green as soon as the necessary jobs are done (currently, CPython 3.6 and 3.7).

--- .ci/install-python.sh
+++ .ci/install-python.sh
@@ -9,13 +9,31 @@ if [[ "$TRAVIS_OS_NAME" != linux ]]; then
    exit 1
 fi
 
-MINICONDA_OS=Linux
-URL="https://repo.continuum.io/miniconda/Miniconda3-latest-${MINICONDA_OS}-x86_64.sh"
-wget "${URL}" -O miniconda.sh
-bash miniconda.sh -b -p "$HOME/miniconda"
-export PATH="$HOME/miniconda/bin:$PATH"
-conda config --set always_yes yes --set changeps1 no
-conda update -q conda
-conda info -a
-conda create -q -n test-environment "python=$PYTHON"
-source activate test-environment
+if [[ "$PYTHON" =~ pypy-* ]]; then
+  # Download & install a prebuilt pypy snapshot
+  PYPY_BRANCH=${PYTHON#*-}
+  URL="http://buildbot.pypy.org/nightly/${PYPY_BRANCH}/pypy-c-jit-latest-linux64.tar.bz2"
+  wget "${URL}" -O pypy.tar.bz2
+  tar -xf pypy.tar.bz2
+  PYPY=( pypy-c-jit-* )
+
+  echo "Using ${PYPY}"
+  export PATH="${PWD}/${PYPY}/bin:${PATH}"
+  ln -s pypy3 "${PYPY}/bin/python"
+  python -m ensurepip
+  ln -s pip3 "${PYPY}/bin/pip"
+  pip install -U pip wheel
+
+else
+  # Install a Python version with miniconda
+  MINICONDA_OS=Linux
+  URL="https://repo.continuum.io/miniconda/Miniconda3-latest-${MINICONDA_OS}-x86_64.sh"
+  wget "${URL}" -O miniconda.sh
+  bash miniconda.sh -b -p "$HOME/miniconda"
+  export PATH="$HOME/miniconda/bin:$PATH"
+  conda config --set always_yes yes --set changeps1 no
+  conda update -q conda
+  conda info -a
+  conda create -q -n test-environment "python=$PYTHON"
+  source activate test-environment
+fi
