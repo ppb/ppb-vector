b49ecba8c0a8ca05351b3eff6c1c244064bb5081
Nicolas Braud-Santoni <nicolas@braud-santoni.eu>
2019-01-15 15:31:54 +0100

    tests/equality: Replace test for != with an Hypothesis test

--- tests/test_vector2_equality.py
+++ tests/test_vector2_equality.py
@@ -1,13 +1,14 @@
-import ppb_vector
+from hypothesis import given
+from ppb_vector import Vector2
+from utils import vectors
 
 def test_equal():
-  test_vector_1 = ppb_vector.Vector2(50, 800)
-  test_vector_2 = ppb_vector.Vector2(50, 800)
+  test_vector_1 = Vector2(50, 800)
+  test_vector_2 = Vector2(50, 800)
 
   assert test_vector_1 == test_vector_2
 
-def test_not_equal():
-  test_vector_1 = ppb_vector.Vector2(800, 800)
-  test_vector_2 = ppb_vector.Vector2(50, 800)
 
-  assert test_vector_1 != test_vector_2
+@given(x=vectors(), y=vectors())
+def test_not_equal_equivalent(x: Vector2, y: Vector2):
+  assert (x != y) == (not x == y)
