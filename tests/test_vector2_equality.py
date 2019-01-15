06d9ada18ef8d201383317e6e0fac078a01ab206
Nicolas Braud-Santoni <nicolas@braud-santoni.eu>
2019-01-15 15:39:07 +0100

    tests/equality: Replace test for == with an Hypothesis test

--- tests/test_vector2_equality.py
+++ tests/test_vector2_equality.py
@@ -1,13 +1,16 @@
-from hypothesis import given
+from hypothesis import assume, given
 from ppb_vector import Vector2
 from utils import vectors
 
-def test_equal():
-  test_vector_1 = Vector2(50, 800)
-  test_vector_2 = Vector2(50, 800)
 
-  assert test_vector_1 == test_vector_2
+@given(x=vectors())
+def test_equal_self(x: Vector2):
+  assert x == x
 
+@given(x=vectors())
+def test_non_zero_equal(x: Vector2):
+  assume(x != (0, 0))
+  assert x != 1.1 * x
 
 @given(x=vectors(), y=vectors())
 def test_not_equal_equivalent(x: Vector2, y: Vector2):
