74abf94eb16a10791346d63e24d82c89946eaa72
Nicolas Braud-Santoni <nicolas@braud-santoni.eu>
2019-03-24 00:52:48 +0100

    Vector2: Remove the copy constructor
    Vector2.convert should systematically be used instead.

--- tests/test_vector2_ctor.py
+++ tests/test_vector2_ctor.py
@@ -20,15 +20,6 @@ def test_ctor_roundtrip(cls, coerce, v: Vector2):
 
 
 @pytest.mark.parametrize("cls", [Vector2, V])
-@given(x=vectors())
-def test_ctor_vector_like(cls, x: Vector2):
-    for x_like in vector_likes(x):
-        vector = cls(x_like)
-        assert vector == x == x_like
-        assert isinstance(vector, cls)
-
-
-@pytest.mark.parametrize("cls", [Vector2, V])
 @given(x=floats(), y=floats())
 def test_ctor_coordinates(cls, x: float, y: float):
-    assert cls(x, y) == cls((x, y))
+    assert cls(x, y) == cls.convert((x, y))
