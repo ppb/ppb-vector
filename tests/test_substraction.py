b4c8f9af6381c2af7788a63d43a0c86ce3816fab
Nicolas Braud-Santoni <nicolas@braud-santoni.eu>
2019-05-09 00:55:14 -0400

    tests/substraction: Remove tests obsoleted by tests/typing
    No need to check the behaviour of operators with vector-likes,
    there is already an Hypothesis test that does it better.

--- tests/test_substraction.py
+++ tests/test_substraction.py
@@ -3,35 +3,11 @@ import pytest  # type: ignore
 from ppb_vector import Vector
 
 
-def test_substraction_vectors():
-    test_vector1 = Vector(0, 1)
-    test_vector2 = Vector(0, 1)
-    result = test_vector1 - test_vector2
-    assert result == Vector(0, 0)
-
-
-def test_substraction_vector_tuple():
-    test_vector = Vector(4, 6)
-    test_tuple = (1, 1)
-    result = test_vector - test_tuple
-    assert result == Vector(3, 5)
-
-
-def test_substraction_vector_list():
-    test_vector = Vector(3, 7)
-    test_list = [1, 3]
-    result = test_vector - test_list
-    assert result == Vector(2, 4)
-
-
-def test_substraction_vector_dict():
-    test_vector = Vector(7, 11)
-    test_dict = {"x": 3, "y": 5}
-    result = test_vector - test_dict
-    assert result == Vector(4, 6)
-
-
 data = [
+    ([Vector(0, 1), Vector(0, 1)], Vector(0, 0)),
+    ([Vector(4, 6), Vector(1, 1)], Vector(3, 5)),
+    ([Vector(3, 7), Vector(1, 3)], Vector(2, 4)),
+    ([Vector(7, 11), Vector(3, 5)], Vector(4, 6)),
     ([Vector(10, 16), Vector(2, 2)], Vector(8, 14)),
     ([Vector(25, 22), Vector(22, 61)], Vector(3, -39)),
     ([Vector(39, 43), Vector(92, -12)], Vector(-53, 55)),
@@ -40,7 +16,6 @@ data = [
     ([Vector(42, 12), (-5, 23)], Vector(47, -11)),
     ([Vector(51, 28), [72, 31]], Vector(-21, -3)),
     ([Vector(1, 2), [2, 2]], Vector(-1, 0)),
-    ([Vector(1, 2), {"x": 2, "y": 2}], Vector(-1, 0)),
 ]
 
 
