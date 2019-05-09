1d3e956dcf667601feb871eab2a462fa09d0d101
Nicolas Braud-Santoni <nicolas@braud-santoni.eu>
2019-05-09 01:36:18 -0400

    tests/length: Test the axioms of normed vector spaces

--- tests/test_length.py
+++ tests/test_length.py
@@ -1,10 +1,10 @@
-from math import sqrt
+from math import fabs, sqrt
 
 import pytest  # type: ignore
 from hypothesis import given
 
 from ppb_vector import Vector
-from utils import isclose, vectors
+from utils import floats, isclose, vectors
 
 
 @pytest.mark.parametrize(
@@ -24,3 +24,21 @@ def test_length(x, y, expected):
 def test_length_dot(v: Vector):
     """Test that |v| ≃ √v²."""
     assert isclose(v.length, sqrt(v * v))
+
+
+@given(v=vectors())
+def test_length_zero(v: Vector):
+    """1st axiom of normed vector spaces: |v| = 0 iff v = 0"""
+    assert (v.length == 0) == (v == (0, 0))
+
+
+@given(v=vectors(), scalar=floats())
+def test_length_scalar(v: Vector, scalar: float):
+    """2nd axiom of normed vector spaces: |λv| = |λ| |v|"""
+    assert isclose((scalar * v).length, fabs(scalar) * v.length)
+
+
+@given(v=vectors(), w=vectors())
+def test_length_triangle(v: Vector, w: Vector):
+    """3rd axiom of normed vector spaces: |v+w| = |v| + |w|"""
+    assert (v + w).length <= v.length + w.length
