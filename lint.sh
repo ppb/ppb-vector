20eb2fe596b823e93cd606bba25c1e350e14a41b
Nicolas Braud-Santoni <nicolas@braud-santoni.eu>
2019-05-09 01:00:26 -0400

    tests/substraction: Uniformize the table of expected results

--- lint.sh
+++ lint.sh
@@ -7,7 +7,7 @@ if [[ "${PYTHON-x}" =~ pypy-* ]]; then
 fi
 
 run flake8 --version
-run flake8 --ignore F403,F405 tests
+run flake8 --ignore E241,F403,F405 tests
 run flake8 --exclude tests/
 
 run ${PY} -m mypy ppb_vector tests
--- tests/test_substraction.py
+++ tests/test_substraction.py
@@ -4,21 +4,21 @@ from ppb_vector import Vector
 
 
 data = [
-    ([Vector(0, 1), Vector(0, 1)], Vector(0, 0)),
-    ([Vector(4, 6), Vector(1, 1)], Vector(3, 5)),
-    ([Vector(3, 7), Vector(1, 3)], Vector(2, 4)),
-    ([Vector(7, 11), Vector(3, 5)], Vector(4, 6)),
-    ([Vector(10, 16), Vector(2, 2)], Vector(8, 14)),
-    ([Vector(25, 22), Vector(22, 61)], Vector(3, -39)),
-    ([Vector(39, 43), Vector(92, -12)], Vector(-53, 55)),
-    ([Vector(1, 1), (2, 2)], Vector(-1, -1)),
-    ([Vector(25, 22), (12, 92)], Vector(13, -70)),
-    ([Vector(42, 12), (-5, 23)], Vector(47, -11)),
-    ([Vector(51, 28), [72, 31]], Vector(-21, -3)),
-    ([Vector(1, 2), [2, 2]], Vector(-1, 0)),
+    ((0, 1),   (0, 1),    (0, 0)),
+    ((4, 6),   (1, 1),    (3, 5)),
+    ((3, 7),   (1, 3),    (2, 4)),
+    ((7, 11),  (3, 5),    (4, 6)),
+    ((10, 16), (2, 2),    (8, 14)),
+    ((25, 22), (22, 61),  (3, -39)),
+    ((39, 43), (92, -12), (-53, 55)),
+    ((1, 1),   (2, 2),    (-1, -1)),
+    ((25, 22), (12, 92),  (13, -70)),
+    ((42, 12), (-5, 23),  (47, -11)),
+    ((51, 28), (72, 31),  (-21, -3)),
+    ((1, 2),   (2, 2),    (-1, 0)),
 ]
 
 
-@pytest.mark.parametrize("test_input, expected", data)
-def test_multiples_values(test_input, expected):
-    assert (test_input[0] - test_input[1]) == expected
+@pytest.mark.parametrize("x, y, expected", data)
+def test_multiples_values(x, y, expected):
+    assert (Vector(x) - y) == expected
