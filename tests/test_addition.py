f9eab6d3a6211f1935190d8259688046480deea7
Nicolas Braud-Santoni <nicolas@braud-santoni.eu>
2019-05-09 01:03:39 -0400

    tests/addition: Uniformize the table of expected results

--- tests/test_addition.py
+++ tests/test_addition.py
@@ -6,21 +6,21 @@ from utils import vector_likes, vectors
 
 
 data = [
-    ([Vector(1, 0), (0, 1)], Vector(1, 1)),
-    ([Vector(1, 1), (2, 2)], Vector(3, 3)),
-    ([Vector(1, 2), [2, 2]], Vector(3, 4)),
-    ([Vector(10, 16), Vector(2, 2)], Vector(12, 18)),
-    ([Vector(25, 22), (12, 92)], Vector(37, 114)),
-    ([Vector(25, 22), Vector(22, 61)], Vector(47, 83)),
-    ([Vector(39, 43), Vector(92, -12)], Vector(131, 31)),
-    ([Vector(42, 12), (-5, 23)], Vector(37, 35)),
-    ([Vector(51, 28), [72, 31]], Vector(123, 59)),
+    ((1, 0),   (0, 1),    (1, 1)),
+    ((1, 1),   (2, 2),    (3, 3)),
+    ((1, 2),   (2, 2),    (3, 4)),
+    ((10, 16), (2, 2),    (12, 18)),
+    ((25, 22), (12, 92),  (37, 114)),
+    ((25, 22), (22, 61),  (47, 83)),
+    ((39, 43), (92, -12), (131, 31)),
+    ((42, 12), (-5, 23),  (37, 35)),
+    ((51, 28), (72, 31),  (123, 59)),
 ]
 
 
-@pytest.mark.parametrize("test_input, expected", data)
-def test_multiples_values(test_input, expected):
-    assert (test_input[0] + test_input[1]) == expected
+@pytest.mark.parametrize("x, y, expected", data)
+def test_multiples_values(x, y, expected):
+    assert (Vector(x) + y) == expected
 
 
 @given(x=vectors(), y=vectors())
