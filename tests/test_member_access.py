fd7f413925491f305a30a73f0c6eb6306a9ebf19
Nicolas Braud-Santoni <nicolas@braud-santoni.eu>
2019-05-09 01:38:50 -0400

    tests/member_access: Make {index.key}_access into Hypothesis tests

--- tests/test_member_access.py
+++ tests/test_member_access.py
@@ -1,6 +1,8 @@
+from hypothesis import given
 import pytest  # type: ignore
 
 from ppb_vector import Vector
+from utils import vectors
 
 
 @pytest.fixture()
@@ -13,11 +15,13 @@ def test_class_member_access(vector):
     assert vector.y == 20
 
 
-def test_index_access(vector):
-    assert vector[0] == 10
-    assert vector[1] == 20
+@given(v=vectors())
+def test_index_access(v: Vector):
+    assert v[0] == v.x
+    assert v[1] == v.y
 
 
-def test_key_access(vector):
-    assert vector["x"] == 10
-    assert vector["y"] == 20
+@given(v=vectors())
+def test_key_access(v: Vector):
+    assert v["x"] == v.x
+    assert v["y"] == v.y
