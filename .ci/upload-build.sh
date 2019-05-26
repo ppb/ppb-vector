0e58bc6b7a836599f1ea1861fe0b4094d5ceef17
Nicolas Braud-Santoni <nicolas@braud-santoni.eu>
2019-05-25 23:12:24 -0400

    Cirrus CI: upload build artifacts to Github and (test) PyPI
    - requirements-upload.txt taken from pursuedpybear;
    - .ci/upload-build.sh inspired by Cirrus CI's documentation;
    - upload task uses account 'pursuedpybear' on Github and test.pypi.org.

---
+++ .ci/upload-build.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+# Pilfered from https://cirrus-ci.org/examples/#release-assets
+set -euo pipefail
+
+if ! [[ -v CIRRUS_RELEASE ]]; then
+	echo "Not a release. No need to deploy!"
+	exit 0
+fi
+
+REQUIRED_VARS=( GITHUB_TOKEN PYPI_{REPOSITORY,USER,PASSWORD} )
+for var in "${REQUIRED_VARS[@]}"; do
+	if ! [[ -v "$var" ]]; then
+		echo "Please set environment variable '$var' !"
+		exit 1
+	fi
+done
+
+
+file_content_type="application/octet-stream"
+files_to_upload=( dist/* )
+
+for fpath in "${files_to_upload[@]}"; do
+	echo "Uploading '$fpath' to Github..."
+	name=$(basename "$fpath")
+	url_to_upload="https://uploads.github.com/repos/$CIRRUS_REPO_FULL_NAME/releases/$CIRRUS_RELEASE/assets?name=$name"
+	curl -X POST \
+		--data-binary @$fpath \
+		--header "Authorization: token $GITHUB_TOKEN" \
+		--header "Content-Type: $file_content_type" \
+		$url_to_upload
+done
+
+twine upload \
+	--repository-url "$PYPI_REPOSITORY" \
+	--username "$PYPI_USERNAME" \
+	--password "$PYPI_PASSWORD" \
+	"${files_to_upload[@]}"
