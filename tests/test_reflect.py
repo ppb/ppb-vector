0c45abb2034c29270aeeae24f716f89793fb669e
Nicolas Braud-Santoni <nicolas@braud-santoni.eu>
2019-05-26 17:33:44 -0400

    tests/reflect: Clean-up and rename test_reflect_prop

--- tests/test_reflect.py
+++ tests/test_reflect.py
@@ -1,7 +1,7 @@
 from math import isinf, isnan
 
 import pytest  # type: ignore
-from hypothesis import assume, given, note
+from hypothesis import assume, given
 
 from ppb_vector import Vector
 from utils import angle_isclose, isclose, units, vectors
@@ -33,29 +33,20 @@ def test_reflect_involutive(initial: Vector, normal: Vector):
 
     initial.reflect(normal).reflect(normal) ≃ initial
     """
-    assert initial.isclose( initial.reflect(normal).reflect(normal) )
+    assert initial.isclose(initial.reflect(normal).reflect(normal))
 
 
 @given(initial=vectors(), normal=units())
-def test_reflect_prop(initial: Vector, normal: Vector):
-    """Test several properties of Vector.reflect
+def test_reflect_angle(initial: Vector, normal: Vector):
+    """Test angle-related properties of Vector.reflect:
 
     * initial.reflect(normal) * normal == - initial * normal
     * normal.angle(initial) == 180 - normal.angle(reflected)
     """
-    # Exclude cases where the initial vector is very close to the surface
+    # Exclude initial vectors that are very small or very close to the surface.
     assume(not angle_isclose(initial.angle(normal) % 180, 90, epsilon=10))
-
-    # Exclude cases where the initial vector is very small
     assume(initial.length > 1e-10)
 
     reflected = initial.reflect(normal)
-    returned = reflected.reflect(normal)
-    note(f"|normal|: {normal.length}, |initial|: {initial.length}")
-    note(f"angle(normal, initial): {normal.angle(initial)}")
-    note(f"angle(normal, reflected): {normal.angle(reflected)}")
-    note(f"Reflected: {reflected}")
-    note(f"initial ⋅ normal: {initial * normal}")
-    note(f"reflected ⋅ normal: {reflected * normal}")
     assert isclose((initial * normal), -(reflected * normal))
     assert angle_isclose(normal.angle(initial), 180 - normal.angle(reflected))
