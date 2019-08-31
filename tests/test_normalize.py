15013c51f602786265b59c1d4a7e894eae090d90
Nicolas Braud-Santoni <nicolas@braud-santoni.eu>
2019-08-31 12:56:09 +0200

    tests/normalize: Test that direction is preserved

--- tests/test_normalize.py
+++ tests/test_normalize.py
@@ -9,3 +9,10 @@ def test_normalize_length(v):
     assume(v)
     assert isclose(v.normalize().length, 1)
     assert v.isclose(v.length * v.normalize())
+
+
+@given(v=vectors())
+def test_normalize_angle(v):
+    """Normalization preserves direction."""
+    assume(v)
+    assert angle_isclose(v.normalize().angle(v), 0)
