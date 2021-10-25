83f0a963a869e996495fdbdc6056122155237afe
nicoo <nicoo@mur.at>
2021-10-25 09:05:01 +0200

    .common.sh: Extract Python version, assert Py3

--- .common.sh
+++ .common.sh
@@ -15,3 +15,7 @@ PY=${PY-python3}
 
 command -v $PY >/dev/null || die "Python interpreter '$PY' not found"
 
+PY_MAJOR="$($PY -c 'import sys; print(sys.version_info.major)')"
+PY_MINOR="$($PY -c 'import sys; print(sys.version_info.minor)')"
+
+[ "${PY_MAJOR}" -eq 3 ] || die Only Python 3 is supported
