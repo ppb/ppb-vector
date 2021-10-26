14365adbd408519f4fd0f0ee6283d99f23d05d0c
nicoo <nicoo@mur.at>
2021-10-26 19:41:22 +0200

    lint.sh: Avoid linting .eggs/ and other directories

--- lint.sh
+++ lint.sh
@@ -10,7 +10,7 @@ run flake8 --version
 run flake8 --ignore E241,F403,F405,B011 \
     $([ $PY_MINOR -ge 10 ] || echo '--exclude tests/test_pattern_matching.py') \
     tests
-run flake8 --exclude tests/
+run flake8 *.py ppb_vector/
 
 # Exclusion due to https://github.com/python/mypy/pull/10191
 run ${PY} -m mypy ppb_vector tests --exclude test_pattern_matching.py
