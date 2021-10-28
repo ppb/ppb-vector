2fb57ec8be468d815e97ffb6e43f00fdd6f7860d
nicoo <nicoo@mur.at>
2021-10-28 12:29:47 +0200

    test.sh: Autoparallelise the testsuite as possible
    On my workstation, this yields a 4× reduction in test time in the dev profile,
    and almost 8× under the CI test profile.  The difference is that Hypothesis run
    each test 10 times (w/ different random inputs) in dev and 1k time in CI.

--- test.sh
+++ test.sh
@@ -9,6 +9,10 @@ else
     PYTEST_OPTIONS=( )
 fi
 
+# Autoparallelise the testsuite when pytest-xdist is available
+if ${PY} -m pip show -q pytest-xdist 2>/dev/null; then
+    PYTEST_OPTIONS+=( -n auto )
+fi
 
 run ${PY} -m doctest README.md ppb_vector/__init__.py
 run ${PY} -m pytest "${PYTEST_OPTIONS[@]}"
