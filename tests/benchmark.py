edb08a214c16591771c2470bf283b59d1a87b8e2
nicoo <nicoo@mur.at>
2021-10-28 14:13:16 +0200

    tests/benchmark: Do not run at import-time
    This was causing some doctest thing to choke.

--- tests/benchmark.py
+++ tests/benchmark.py
@@ -4,16 +4,17 @@ import pyperf  # type: ignore
 from ppb_vector import Vector
 from utils import *
 
-r = pyperf.Runner()
-x = Vector(1, 1)
-y = Vector(0, 1)
-scalar = 123
+if __name__ == "__main__":
+    r = pyperf.Runner()
+    x = Vector(1, 1)
+    y = Vector(0, 1)
+    scalar = 123
 
-for f in BINARY_OPS + BINARY_SCALAR_OPS + BOOL_OPS:  # type: ignore
-    r.bench_func(f.__name__, f, x, y)
+    for f in BINARY_OPS + BINARY_SCALAR_OPS + BOOL_OPS:  # type: ignore
+        r.bench_func(f.__name__, f, x, y)
 
-for f in UNARY_OPS + UNARY_SCALAR_OPS:  # type: ignore
-    r.bench_func(f.__name__, f, x)
+    for f in UNARY_OPS + UNARY_SCALAR_OPS:  # type: ignore
+        r.bench_func(f.__name__, f, x)
 
-for f in SCALAR_OPS:  # type: ignore
-    r.bench_func(f.__name__, f, x, scalar)  # type: ignore
+    for f in SCALAR_OPS:  # type: ignore
+        r.bench_func(f.__name__, f, x, scalar)  # type: ignore
