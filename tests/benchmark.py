57d302b3ffa285d482577b4265aebb0463404651
nicoo <nicoo@mur.at>
2021-10-29 10:18:53 +0200

    bench: Minor refactor

--- tests/benchmark.py
+++ tests/benchmark.py
@@ -4,17 +4,23 @@ import pyperf  # type: ignore
 from ppb_vector import Vector
 from utils import *
 
-if __name__ == "__main__":
-    r = pyperf.Runner()
-    x = Vector(1, 1)
-    y = Vector(0, 1)
-    scalar = 123
 
-    for f in BINARY_OPS + BINARY_SCALAR_OPS + BOOL_OPS:  # type: ignore
-        r.bench_func(f.__name__, f, x, y)
+X = Vector(1, 1)
+Y = Vector(0, 1)
+λ = 123
+
+
+def benches(r: pyperf.Runner):
+   for f in BINARY_OPS + BINARY_SCALAR_OPS + BOOL_OPS:  # type: ignore
+        r.bench_func(f.__name__, f, X, Y)
 
     for f in UNARY_OPS + UNARY_SCALAR_OPS:  # type: ignore
-        r.bench_func(f.__name__, f, x)
+        r.bench_func(f.__name__, f, X)
 
     for f in SCALAR_OPS:  # type: ignore
-        r.bench_func(f.__name__, f, x, scalar)  # type: ignore
+        r.bench_func(f.__name__, f, X, λ)  # type: ignore
+
+
+if __name__ == "__main__":
+    r = pyperf.Runner()
+    benches(r)
