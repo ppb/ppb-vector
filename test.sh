a1a8da8a38408c061641133fa38496c0becd14ec
nicoo <nicoo@mur.at>
2021-11-01 14:16:22 +0100

    test.sh: Limit the degree of parallelism in Linux CI
    Apparently, pytext-xdist wrongly detects there being 30 cores available (what
    the host's CPU supports) rather than the default 2 available to the container
    by default.

--- test.sh
+++ test.sh
@@ -1,18 +1,20 @@
 #!/usr/bin/env bash
 source .common.sh
 
+PYTEST_OPTIONS=()
+
 if [[ "${CI+x}" == x ]]; then
-    IN_CI=1
-    PYTEST_OPTIONS=( --hypothesis-profile ci )
-else
-    IN_CI=0
-    PYTEST_OPTIONS=( )
+		# Use a CI-specific profile for Hypothesis
+		PYTEST_OPTIONS+=( --hypothesis-profile ci )
+
+		# Autodetect the level of parallelism if unspecified
+		PYTEST_OPTIONS+=( -n "${PYTEST_CPUS-auto}" )
+
+elif ${PY} -m pip show -q pytest-xdist 2>/dev/null; then
+		# Autoparallelise the testsuite when pytest-xdist is available
+		PYTEST_OPTIONS=( -n auto )
 fi
 
-# Autoparallelise the testsuite when pytest-xdist is available
-if ${PY} -m pip show -q pytest-xdist 2>/dev/null; then
-    PYTEST_OPTIONS+=( -n auto )
-fi
 
 run ${PY} -m doctest README.md ppb_vector/__init__.py
 run ${PY} -m pytest "${PYTEST_OPTIONS[@]}"
